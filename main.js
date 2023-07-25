43d1a57aed5d26fbe00c51e31f26b2debc8b756e
Dániel <33530616+danielxszilagyi@users.noreply.github.com>
2023-07-25 23:28:17 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+/* */
+// gomb
+let submitbutton = document.querySelector("button");
+submitbutton.onclick = () => {
+  costOfTravel.dataIn();
+};
+// fogyasztasszamolo OBJ
+let costOfTravel = {
+  km: null,
+  gasPrice: null,
+  consumption: null,
+  calcOfTravelCost: function () {
+    this.sum = Math.round((this.km / 100) * this.consumption * this.gasPrice);
+    return this.sum;
+  },
+  dataIn: function () {
+    this.km = document.getElementById("tav").value;
+    this.consumption = document.getElementById("fogyasztas").value;
+    this.gasPrice = document.getElementById("uzemanyagAr").value;
+    if (this.km && this.consumption && this.gasPrice > 0) {
+      costOfTravel.calcOfTravelCost();
+      costOfTravel.dataOut();
+    } else {
+      alert(`Add meg az adatokat!`);
+    }
+  },
+  sum: 0,
+  dataOut: function () {
+    let finalOut = document.getElementById("eredmeny");
+    let result = this.sum.toLocaleString();
+    finalOut.textContent = `Az útiköltség: ${result}ft`;
+  },
+};
